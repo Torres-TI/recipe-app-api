6c236e90b3f3a5f8112c6607161115346198f6b5
Torres-TI <developertorress@gmail.com>
2025-04-23 18:31:24 -0700

    added tags and created test for update tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,16 +1,20 @@
 """Views for the Recipe API"""
 
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets,
+    mixins
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag
+)
 from recipe import serializers
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
-
     serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
@@ -18,7 +22,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by("-id")
+        return self.queryset\
+            .filter(user=self.request.user)\
+            .order_by("-id")
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
@@ -29,3 +35,21 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    """Manage Tags in the database"""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
+        return self.queryset\
+            .filter(user=self.request.user)\
+            .order_by('-name')
